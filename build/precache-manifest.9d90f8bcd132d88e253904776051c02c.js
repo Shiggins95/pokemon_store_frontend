9f35756a921316b1412986b8a28492b003fa6f38
Stephen Higgins <stephen.higgins@cofficient.co.uk>
2021-01-18 01:01:31 +0000

    updates for digiocean

---
+++ build/precache-manifest.9d90f8bcd132d88e253904776051c02c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8850ed162929fdb70180da35c84d65e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9927cfa09554194b9157",
+    "url": "/static/css/main.0865077f.chunk.css"
+  },
+  {
+    "revision": "2bcb7901b3fa1daa08db",
+    "url": "/static/js/2.1d769b0a.chunk.js"
+  },
+  {
+    "revision": "9927cfa09554194b9157",
+    "url": "/static/js/main.a5f8c182.chunk.js"
+  },
+  {
+    "revision": "735327a527ca36b7fcf7",
+    "url": "/static/js/runtime-main.8c2ce1a5.js"
+  }
+]);
